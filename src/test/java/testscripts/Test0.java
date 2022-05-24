dcad8a0d5d6898ecdf8508f20ace278f239dff2f
91707 <91707@Shilpa-Srivastava>
2022-05-24 11:36:00 +0530

    second commit

---
+++ src/test/java/testscripts/Test0.java
@@ -0,0 +1,13 @@
+package testscripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test0 {
+	
+@Test
+public void tc1()
+{
+	Reporter.log("Hello Java",true);
+}
+}
---
+++ src/test/java/testscripts/Test1.java
@@ -0,0 +1,13 @@
+package testscripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	@Test
+	public void tc2()
+	{
+		Reporter.log(" Hello Selenium", true);
+	}
+
+}
